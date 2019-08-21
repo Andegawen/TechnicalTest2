8265b50d1687e9ed25c500823f467fb1b8e2a82b
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2019-08-21 20:03:34 +0100

    Introduce FileAbstraction

---
+++ PP.Wpf/FileAbstraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PP.Wpf
+{
+    public class FileAbstraction : IFileAbstraction
+    {
+        public IEnumerable<string> EnumerateTopDirectories(string path)
+        {
+            return Directory.EnumerateDirectories("", "*", SearchOption.TopDirectoryOnly);
+        }
+
+        public IEnumerable<string> EnumerateFilesInDirectory(string path)
+        {
+            return Directory.EnumerateFiles("", "*", SearchOption.TopDirectoryOnly);
+        }
+
+        public IEnumerable<string> GetDrives()
+        {
+            return DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Fixed).Select(d => d.RootDirectory.FullName);
+        }
+    }
+}
---
+++ PP.Wpf/IFileAbstraction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PP.Wpf
+{
+    public interface IFileAbstraction
+    {
+        IEnumerable<string> EnumerateTopDirectories(string path);
+        IEnumerable<string> EnumerateFilesInDirectory(string path);
+        IEnumerable<string> GetDrives();
+    }
+}
